14784ebf5252092cf4436143748ef56d0cd4c4b8
Ferris Schwanebeck <accounts@ferris-s.de>
2024-03-09 00:10:03 +0100

    separated cookie domain into its own file

---
+++ internal/cookie_domain.go
@@ -0,0 +1,74 @@
+package tfa
+
+import (
+	"fmt"
+	"strings"
+)
+
+// CookieDomain holds cookie domain info
+type CookieDomain struct {
+	Domain       string
+	DomainLen    int
+	SubDomain    string
+	SubDomainLen int
+}
+
+// NewCookieDomain creates a new CookieDomain from the given domain string
+func NewCookieDomain(domain string) *CookieDomain {
+	return &CookieDomain{
+		Domain:       domain,
+		DomainLen:    len(domain),
+		SubDomain:    fmt.Sprintf(".%s", domain),
+		SubDomainLen: len(domain) + 1,
+	}
+}
+
+// Match checks if the given host matches this CookieDomain
+func (c *CookieDomain) Match(host string) bool {
+	// Exact domain match?
+	if host == c.Domain {
+		return true
+	}
+
+	// Subdomain match?
+	if len(host) >= c.SubDomainLen && host[len(host)-c.SubDomainLen:] == c.SubDomain {
+		return true
+	}
+
+	return false
+}
+
+// UnmarshalFlag converts a string to a CookieDomain
+func (c *CookieDomain) UnmarshalFlag(value string) error {
+	*c = *NewCookieDomain(value)
+	return nil
+}
+
+// MarshalFlag converts a CookieDomain to a string
+func (c *CookieDomain) MarshalFlag() (string, error) {
+	return c.Domain, nil
+}
+
+// CookieDomains provides legacy sypport for comma separated list of cookie domains
+type CookieDomains []CookieDomain
+
+// UnmarshalFlag converts a comma separated list of cookie domains to an array
+// of CookieDomains
+func (c *CookieDomains) UnmarshalFlag(value string) error {
+	if len(value) > 0 {
+		for _, d := range strings.Split(value, ",") {
+			cookieDomain := NewCookieDomain(d)
+			*c = append(*c, *cookieDomain)
+		}
+	}
+	return nil
+}
+
+// MarshalFlag converts an array of CookieDomain to a comma seperated list
+func (c *CookieDomains) MarshalFlag() (string, error) {
+	var domains []string
+	for _, d := range *c {
+		domains = append(domains, d.Domain)
+	}
+	return strings.Join(domains, ","), nil
+}
