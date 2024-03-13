dc5e14715091a7b1b463579ecffc8e549c6cd831
Ferris Schwanebeck <accounts@ferris-s.de>
2024-03-13 23:25:10 +0100

    extracted rule from config

--- internal/config.go
+++ internal/config.go
@@ -328,34 +328,3 @@ func (c *Config) setupProvider(name string) error {
 
 	return nil
 }
-
-// Rule holds defined rules
-type Rule struct {
-	Action    string
-	Rule      string
-	Provider  string
-	Whitelist types.CommaSeparatedList
-	Domains   types.CommaSeparatedList
-}
-
-// NewRule creates a new rule object
-func NewRule() *Rule {
-	return &Rule{
-		Action: "auth",
-	}
-}
-
-func (r *Rule) formattedRule() string {
-	// Traefik implements their own "Host" matcher and then offers "HostRegexp"
-	// to invoke the mux "Host" matcher. This ensures the mux version is used
-	return strings.ReplaceAll(r.Rule, "Host(", "HostRegexp(")
-}
-
-// Validate validates a rule
-func (r *Rule) Validate(c *Config) error {
-	if r.Action != "auth" && r.Action != "soft-auth" && r.Action != "allow" {
-		return errors.New("invalid rule action, must be \"auth\", \"soft-auth\", or \"allow\"")
-	}
-
-	return c.setupProvider(r.Provider)
-}
