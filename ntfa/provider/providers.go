07c9555f29657bc21b24ec3e4e7b3f05b713cd7a
Ferris Schwanebeck <accounts@ferris-s.de>
2024-03-16 14:40:56 +0100

    moved oauth provider into own file

--- ntfa/provider/providers.go
+++ ntfa/provider/providers.go
@@ -2,12 +2,10 @@ package provider
 
 import (
 	"bytes"
-	"context"
 	"fmt"
 	"io"
 
 	"github.com/tidwall/gjson"
-	"golang.org/x/oauth2"
 )
 
 // Providers contains all the implemented providers
@@ -52,43 +50,4 @@ func GetUserFromBytes(jsonBytes []byte, UserPath string) (string, error) {
 		return "", fmt.Errorf("no such user path: '%s' in the UserURL response: %s", UserPath, string(jsonBytes))
 	}
 	return gjResult.String(), nil
-}
-
-// OAuthProvider is a provider using the oauth2 library
-type OAuthProvider struct {
-	Scopes   []string `long:"scope" env:"SCOPE" env-delim:"," default:"profile" default:"email" description:"Scopes"`
-	Prompt   string   `long:"prompt" env:"PROMPT" description:"Optional prompt query"`
-	Resource string   `long:"resource" env:"RESOURCE" description:"Optional resource indicator"`
-
-	Config *oauth2.Config
-	ctx    context.Context
-}
-
-// ConfigCopy returns a copy of the oauth2 config with the given redirectURI
-// which ensures the underlying config is not modified
-func (p *OAuthProvider) ConfigCopy(redirectURI string) oauth2.Config {
-	config := *p.Config
-	config.RedirectURL = redirectURI
-	return config
-}
-
-// OAuthGetLoginURL provides a base "GetLoginURL" for proiders using OAauth2
-func (p *OAuthProvider) OAuthGetLoginURL(redirectURI, state string, forcePrompt bool) string {
-	config := p.ConfigCopy(redirectURI)
-
-	var options []oauth2.AuthCodeOption
-	if p.Prompt != "" && !forcePrompt {
-		options = append(options, oauth2.SetAuthURLParam("prompt", p.Prompt))
-	}
-	if p.Resource != "" {
-		options = append(options, oauth2.SetAuthURLParam("resource", p.Resource))
-	}
-
-	return config.AuthCodeURL(state, options...)
-}
-
-// OAuthExchangeCode provides a base "ExchangeCode" for proiders using OAauth2
-func (p *OAuthProvider) OAuthExchangeCode(redirectURI, code string) (*oauth2.Token, error) {
-	config := p.ConfigCopy(redirectURI)
-	return config.Exchange(p.ctx, code)
 }
